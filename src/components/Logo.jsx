c7a24621002718473e97a3491154c9b764a9395d
lcasan <lcasan120100@gmail.com>
2024-11-04 11:15:33 -0500

    Logo section created

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import "./Logo.css"
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src="src/assets/icons/virtual-market.svg" alt="Logo" role="img" />
+            <h1>Tienda Virtual</h1>
+        </div>
+    );
+};
+
+export {Logo};
