6f461063074afc3f62496b8d1cfe94dbcf0f39aa
lcasan <lcasan120100@gmail.com>
2024-11-05 14:09:26 -0500

    Refactor Table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,10 +1,23 @@
 import { DataContext } from "../context/DataContext";
+import { ProductList } from "./ProductList";
+import { NotProduct } from "./NotProduct";
+import { ProductForm } from "./ProductForm";
 import { useContext, useState } from "react";
 import "./Table.css";
-import { Product } from "./Product";
 
 const Table = () => {
-  const data = useContext(DataContext);
+  const { data, setData } = useContext(DataContext);
+  const [search, setSearch] = useState(""); // Search term
+  const [showCreateForm, setShowCreateForm] = useState(false);
+
+  // Filter:
+  const handleSearch = (evt) => {
+    setSearch(evt.target.value.toLowerCase());
+  };
+
+  const searchResult = data.filter((product) => {
+    return product.name.toLowerCase().includes(search); // Search by name
+  });
 
   return (
     <div className="table-container">
@@ -17,12 +30,26 @@ const Table = () => {
             <th scope="col">Tipo</th>
             <th scope="col">Costo de envío</th>
             <th scope="col">Link de descarga</th>
+            <th colSpan={2}>
+              <input
+                type="text"
+                onChange={handleSearch}
+                placeholder="Buscar producto..."
+              />
+            </th>
+            <th>
+              <button onClick={() => setShowCreateForm(!showCreateForm)}>
+                {showCreateForm ? "Cancelar" : "Crear Producto"}
+              </button>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {data.map((product) => (
-            <Product product={product} />
-          ))}
+          {/* Form for create product */}
+          {showCreateForm && <ProductForm setShowCreateForm={setShowCreateForm}/> }
+
+          {/* List products search results */}
+          {searchResult.length > 0 ? <ProductList products={searchResult}/> : <NotProduct/>}
         </tbody>
       </table>
     </div>
