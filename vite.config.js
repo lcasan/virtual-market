9f9ad77f875fe67ef3e3ac7dc5eda10b07f1de50
lcasan <lcasan120100@gmail.com>
2024-11-07 13:52:48 -0500

    Set the port dynamically from environment variable
    - Configure server to use PORT from process.env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vite.dev/config/
+dotenv.config()
+const PORT = process.env.PORT;
+
 export default defineConfig({
+  server: {
+    port: PORT,
+  },
   plugins: [react()],
 })
